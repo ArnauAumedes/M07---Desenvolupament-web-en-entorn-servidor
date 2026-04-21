78e3bca8ed43bd4d46d0680c1000149561b3ca70
a.aumedes <a.aumedes@sapalomera.cat>
2026-04-21 11:30:33 +0200

    FIX(): Remove error reporting settings from multiple view files for cleaner production environment

--- app/vista/osm/mejores-valorados.php
+++ app/vista/osm/mejores-valorados.php
@@ -22,9 +22,6 @@ Autor: Arnau Aumedes Jimenez
 
 <body>
     <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
 
     // Incloure capçalera i fitxers necessaris
     require_once __DIR__ . '/../globals/header.php';
